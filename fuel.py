a6a8361acb47527e257161baf7b1abe5ca11ec9f
unknown <joshua.allan92@gmail.com>
2021-06-23 08:09:09 +1000

    refactor check_NSW to remove magic 2

--- fuel.py
+++ fuel.py
@@ -2,25 +2,31 @@ import json
 import urllib3
 import pprint
 
-def checkNSW():
+def check_NSW():
     #poll the API and collect results for the states
     
     url = 'https://projectzerothree.info/api.php?format=json'
     http = urllib3.PoolManager()
     r = http.request('GET', url)
     fuel = json.loads(r.data)
-    NSW = 2
-    return fuel["regions"][NSW]["prices"]
+    NSW = fuel["regions"]["prices"]
+    for state in NSW:
+        if state["region"] == "NSW":
+            return state    
+
+    # return fuel["regions"][NSW]["prices"]
 
 def get_NSW_Suburb_List():
     #call CheckNSW to check for single store to test logic
-    suburb_list = checkNSW()
+    suburb_list = check_NSW()
     #iterate over list using known values
-    for suburb in suburb_list:
-        if suburb["suburb"] == "Revesby":
-            return suburb
-
-
-
+    if suburb_list == ["Revesby"] or ["Mayfield"] or ["Lurnea"]:
+        return suburb_list
+        
+def get_Store_Prices():
+    store_prices = get_NSW_Suburb_List()
+    for store in store_prices:
+        print(store["price"])
     
-pprint.pprint(get_NSW_Suburb_List())
+# print(get_NSW_Suburb_List(), get_Store_Prices())
+print(get_Store_Prices())
