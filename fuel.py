b4098556126a014944d3cffdffd3bd01bd538c6e
unknown <joshua.allan92@gmail.com>
2021-06-22 23:53:56 +1000

    update

--- fuel.py
+++ fuel.py
@@ -2,11 +2,7 @@ import json
 import urllib3
 import pprint
 
-<<<<<<< HEAD
 def check_NSW():
-=======
-def checkNSW():
->>>>>>> cb733ef31d7e9962a2c9b12a915eb7a165fa0606
     #poll the API and collect results for the states
     
     url = 'https://projectzerothree.info/api.php?format=json'
@@ -18,7 +14,6 @@ def checkNSW():
 
 def get_NSW_Suburb_List():
     #call CheckNSW to check for single store to test logic
-<<<<<<< HEAD
     suburb_list = check_NSW()
     #iterate over list using known values
     if suburb_list == ["Revesby"] or ["Mayfield"] or ["Lurnea"]:
@@ -27,18 +22,6 @@ def get_NSW_Suburb_List():
 def get_Store_Prices():
     store_prices = get_NSW_Suburb_List()
     for store in store_prices:
-        print(store["price"])
+        return store
     
 print(get_NSW_Suburb_List(), get_Store_Prices())
-=======
-    suburb_list = checkNSW()
-    #iterate over list using known values
-    for suburb in suburb_list:
-        if suburb["suburb"] == "Revesby":
-            return suburb
-
-
-
-    
-pprint.pprint(get_NSW_Suburb_List())
->>>>>>> cb733ef31d7e9962a2c9b12a915eb7a165fa0606
