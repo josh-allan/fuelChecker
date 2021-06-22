aed1d9cf925b13bdaa0c27e58347674723b91dcb
unknown <joshua.allan92@gmail.com>
2021-06-22 23:21:23 +1000

    price function, update iterables to known values

--- fuel.py
+++ fuel.py
@@ -2,7 +2,7 @@ import json
 import urllib3
 import pprint
 
-def checkState():
+def check_NSW():
     #poll the API and collect results for the states
     
     url = 'https://projectzerothree.info/api.php?format=json'
@@ -12,10 +12,16 @@ def checkState():
     NSW = 2
     return fuel["regions"][NSW]["prices"]
 
-
-# def checkPrice():
-#     location = checkNSW()
-#     price = location
-#     return price
-
-pprint.pprint(checkState())
+def get_NSW_Suburb_List():
+    #call CheckNSW to check for single store to test logic
+    suburb_list = check_NSW()
+    #iterate over list using known values
+    if suburb_list == ["Revesby"] or ["Mayfield"] or ["Lurnea"]:
+        return suburb_list
+        
+def get_Store_Prices():
+    store_prices = get_NSW_Suburb_List()
+    for store in store_prices:
+        print(store["price"])
+    
+print(get_NSW_Suburb_List(), get_Store_Prices())
