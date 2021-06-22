187fae59bd4e9c578ae85d1905075dde78388a58
unknown <joshua.allan92@gmail.com>
2021-06-22 21:38:35 +1000

    working function to search a known store

--- fuel.py
+++ fuel.py
@@ -2,7 +2,7 @@ import json
 import urllib3
 import pprint
 
-def checkState():
+def checkNSW():
     #poll the API and collect results for the states
     
     url = 'https://projectzerothree.info/api.php?format=json'
@@ -12,10 +12,13 @@ def checkState():
     NSW = 2
     return fuel["regions"][NSW]["prices"]
 
+def suburb():
+    #call CheckNSW to check for single store to test logic
+    suburb = checkNSW()
+    #iterate over list using known values
+    for i in suburb:
+        if i["suburb"] == "Revesby":
+            return i
+        
 
-# def checkPrice():
-#     location = checkNSW()
-#     price = location
-#     return price
-
-pprint.pprint(checkState())
+pprint.pprint(suburb())
